d2d64a0b58923322cb66c9a27b8f7d519b219d70
twosoulz <c.lotoz@hotmail.de>
2017-10-25 09:11:20 +0200

    25_10 Hauptfenster Erweitert

---
+++ Projekt_Buecherei_Lauffen/Hauptfenster_Erweitert.Designer.cs
@@ -0,0 +1,393 @@
+﻿namespace Projekt_Buecherei_Lauffen
+{
+    partial class frmHauptfenster_Erweitert
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnReservieren_erw = new System.Windows.Forms.Button();
+            this.lblReserviert_erw = new System.Windows.Forms.Label();
+            this.lblAusgeliehen_erw = new System.Windows.Forms.Label();
+            this.lblisbn_erw = new System.Windows.Forms.Label();
+            this.lblGenre_erw = new System.Windows.Forms.Label();
+            this.lbErgebnis_erw = new System.Windows.Forms.ListBox();
+            this.btnSuchen_erw = new System.Windows.Forms.Button();
+            this.btnAusloggen_erw = new System.Windows.Forms.Button();
+            this.txbSuche_erw = new System.Windows.Forms.TextBox();
+            this.lblJahr_erw = new System.Windows.Forms.Label();
+            this.lblVerlag_erw = new System.Windows.Forms.Label();
+            this.lblTitel_erw = new System.Windows.Forms.Label();
+            this.lblAutor_erw = new System.Windows.Forms.Label();
+            this.lblSuche = new System.Windows.Forms.Label();
+            this.lblBenutzer_erw = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.btnReservierung_loeschen_erw = new System.Windows.Forms.Button();
+            this.lblBenutzer_eingeloggt_erw = new System.Windows.Forms.Label();
+            this.lblAusgeliehen_Ausgabe_erw = new System.Windows.Forms.Label();
+            this.lblReserviert_Ausgabe_erw = new System.Windows.Forms.Label();
+            this.btnAendern_erw = new System.Windows.Forms.Button();
+            this.btnLoeschen_erw = new System.Windows.Forms.Button();
+            this.btnNeu_erw = new System.Windows.Forms.Button();
+            this.btnAusleihen_erw = new System.Windows.Forms.Button();
+            this.btnZurueck_erw = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnReservieren_erw
+            // 
+            this.btnReservieren_erw.Location = new System.Drawing.Point(633, 569);
+            this.btnReservieren_erw.Name = "btnReservieren_erw";
+            this.btnReservieren_erw.Size = new System.Drawing.Size(75, 23);
+            this.btnReservieren_erw.TabIndex = 51;
+            this.btnReservieren_erw.Text = "Reservieren";
+            this.btnReservieren_erw.UseVisualStyleBackColor = true;
+            // 
+            // lblReserviert_erw
+            // 
+            this.lblReserviert_erw.AutoSize = true;
+            this.lblReserviert_erw.Location = new System.Drawing.Point(640, 522);
+            this.lblReserviert_erw.Name = "lblReserviert_erw";
+            this.lblReserviert_erw.Size = new System.Drawing.Size(58, 13);
+            this.lblReserviert_erw.TabIndex = 49;
+            this.lblReserviert_erw.Text = "Reserviert:";
+            // 
+            // lblAusgeliehen_erw
+            // 
+            this.lblAusgeliehen_erw.AutoSize = true;
+            this.lblAusgeliehen_erw.Location = new System.Drawing.Point(640, 472);
+            this.lblAusgeliehen_erw.Name = "lblAusgeliehen_erw";
+            this.lblAusgeliehen_erw.Size = new System.Drawing.Size(68, 13);
+            this.lblAusgeliehen_erw.TabIndex = 47;
+            this.lblAusgeliehen_erw.Text = "Ausgeliehen:";
+            // 
+            // lblisbn_erw
+            // 
+            this.lblisbn_erw.AutoSize = true;
+            this.lblisbn_erw.Location = new System.Drawing.Point(434, 522);
+            this.lblisbn_erw.Name = "lblisbn_erw";
+            this.lblisbn_erw.Size = new System.Drawing.Size(35, 13);
+            this.lblisbn_erw.TabIndex = 45;
+            this.lblisbn_erw.Text = "ISBN:";
+            // 
+            // lblGenre_erw
+            // 
+            this.lblGenre_erw.AutoSize = true;
+            this.lblGenre_erw.Location = new System.Drawing.Point(229, 522);
+            this.lblGenre_erw.Name = "lblGenre_erw";
+            this.lblGenre_erw.Size = new System.Drawing.Size(39, 13);
+            this.lblGenre_erw.TabIndex = 43;
+            this.lblGenre_erw.Text = "Genre:";
+            // 
+            // lbErgebnis_erw
+            // 
+            this.lbErgebnis_erw.FormattingEnabled = true;
+            this.lbErgebnis_erw.Location = new System.Drawing.Point(26, 95);
+            this.lbErgebnis_erw.Name = "lbErgebnis_erw";
+            this.lbErgebnis_erw.Size = new System.Drawing.Size(822, 355);
+            this.lbErgebnis_erw.TabIndex = 41;
+            // 
+            // btnSuchen_erw
+            // 
+            this.btnSuchen_erw.Location = new System.Drawing.Point(432, 56);
+            this.btnSuchen_erw.Name = "btnSuchen_erw";
+            this.btnSuchen_erw.Size = new System.Drawing.Size(75, 23);
+            this.btnSuchen_erw.TabIndex = 40;
+            this.btnSuchen_erw.Text = "Suchen";
+            this.btnSuchen_erw.UseVisualStyleBackColor = true;
+            // 
+            // btnAusloggen_erw
+            // 
+            this.btnAusloggen_erw.Location = new System.Drawing.Point(241, 20);
+            this.btnAusloggen_erw.Name = "btnAusloggen_erw";
+            this.btnAusloggen_erw.Size = new System.Drawing.Size(75, 23);
+            this.btnAusloggen_erw.TabIndex = 39;
+            this.btnAusloggen_erw.Text = "Ausloggen";
+            this.btnAusloggen_erw.UseVisualStyleBackColor = true;
+            // 
+            // txbSuche_erw
+            // 
+            this.txbSuche_erw.Location = new System.Drawing.Point(70, 58);
+            this.txbSuche_erw.Name = "txbSuche_erw";
+            this.txbSuche_erw.Size = new System.Drawing.Size(333, 20);
+            this.txbSuche_erw.TabIndex = 38;
+            // 
+            // lblJahr_erw
+            // 
+            this.lblJahr_erw.AutoSize = true;
+            this.lblJahr_erw.Location = new System.Drawing.Point(29, 522);
+            this.lblJahr_erw.Name = "lblJahr_erw";
+            this.lblJahr_erw.Size = new System.Drawing.Size(30, 13);
+            this.lblJahr_erw.TabIndex = 35;
+            this.lblJahr_erw.Text = "Jahr:";
+            // 
+            // lblVerlag_erw
+            // 
+            this.lblVerlag_erw.AutoSize = true;
+            this.lblVerlag_erw.Location = new System.Drawing.Point(429, 472);
+            this.lblVerlag_erw.Name = "lblVerlag_erw";
+            this.lblVerlag_erw.Size = new System.Drawing.Size(40, 13);
+            this.lblVerlag_erw.TabIndex = 33;
+            this.lblVerlag_erw.Text = "Verlag:";
+            // 
+            // lblTitel_erw
+            // 
+            this.lblTitel_erw.AutoSize = true;
+            this.lblTitel_erw.Location = new System.Drawing.Point(238, 472);
+            this.lblTitel_erw.Name = "lblTitel_erw";
+            this.lblTitel_erw.Size = new System.Drawing.Size(30, 13);
+            this.lblTitel_erw.TabIndex = 31;
+            this.lblTitel_erw.Text = "Titel:";
+            // 
+            // lblAutor_erw
+            // 
+            this.lblAutor_erw.AutoSize = true;
+            this.lblAutor_erw.Location = new System.Drawing.Point(29, 472);
+            this.lblAutor_erw.Name = "lblAutor_erw";
+            this.lblAutor_erw.Size = new System.Drawing.Size(35, 13);
+            this.lblAutor_erw.TabIndex = 29;
+            this.lblAutor_erw.Text = "Autor:";
+            // 
+            // lblSuche
+            // 
+            this.lblSuche.AutoSize = true;
+            this.lblSuche.Location = new System.Drawing.Point(23, 61);
+            this.lblSuche.Name = "lblSuche";
+            this.lblSuche.Size = new System.Drawing.Size(41, 13);
+            this.lblSuche.TabIndex = 28;
+            this.lblSuche.Text = "Suche:";
+            // 
+            // lblBenutzer_erw
+            // 
+            this.lblBenutzer_erw.AutoSize = true;
+            this.lblBenutzer_erw.Location = new System.Drawing.Point(23, 25);
+            this.lblBenutzer_erw.Name = "lblBenutzer_erw";
+            this.lblBenutzer_erw.Size = new System.Drawing.Size(78, 13);
+            this.lblBenutzer_erw.TabIndex = 26;
+            this.lblBenutzer_erw.Text = "Benutzername:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(70, 469);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(139, 20);
+            this.textBox2.TabIndex = 52;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(274, 469);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(139, 20);
+            this.textBox3.TabIndex = 53;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(475, 469);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(139, 20);
+            this.textBox4.TabIndex = 54;
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(70, 519);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(139, 20);
+            this.textBox5.TabIndex = 55;
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(274, 519);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(139, 20);
+            this.textBox6.TabIndex = 56;
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(475, 519);
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(139, 20);
+            this.textBox7.TabIndex = 57;
+            // 
+            // btnReservierung_loeschen_erw
+            // 
+            this.btnReservierung_loeschen_erw.Location = new System.Drawing.Point(714, 569);
+            this.btnReservierung_loeschen_erw.Name = "btnReservierung_loeschen_erw";
+            this.btnReservierung_loeschen_erw.Size = new System.Drawing.Size(134, 23);
+            this.btnReservierung_loeschen_erw.TabIndex = 58;
+            this.btnReservierung_loeschen_erw.Text = "Reservierung löschen";
+            this.btnReservierung_loeschen_erw.UseVisualStyleBackColor = true;
+            // 
+            // lblBenutzer_eingeloggt_erw
+            // 
+            this.lblBenutzer_eingeloggt_erw.AutoSize = true;
+            this.lblBenutzer_eingeloggt_erw.Location = new System.Drawing.Point(107, 25);
+            this.lblBenutzer_eingeloggt_erw.Name = "lblBenutzer_eingeloggt_erw";
+            this.lblBenutzer_eingeloggt_erw.Size = new System.Drawing.Size(123, 13);
+            this.lblBenutzer_eingeloggt_erw.TabIndex = 59;
+            this.lblBenutzer_eingeloggt_erw.Text = "Benutzername_Ausgabe";
+            // 
+            // lblAusgeliehen_Ausgabe_erw
+            // 
+            this.lblAusgeliehen_Ausgabe_erw.AutoSize = true;
+            this.lblAusgeliehen_Ausgabe_erw.Location = new System.Drawing.Point(717, 472);
+            this.lblAusgeliehen_Ausgabe_erw.Name = "lblAusgeliehen_Ausgabe_erw";
+            this.lblAusgeliehen_Ausgabe_erw.Size = new System.Drawing.Size(136, 13);
+            this.lblAusgeliehen_Ausgabe_erw.TabIndex = 60;
+            this.lblAusgeliehen_Ausgabe_erw.Text = "Ausgeliehen_Ausgabe_erw";
+            // 
+            // lblReserviert_Ausgabe_erw
+            // 
+            this.lblReserviert_Ausgabe_erw.AutoSize = true;
+            this.lblReserviert_Ausgabe_erw.Location = new System.Drawing.Point(717, 522);
+            this.lblReserviert_Ausgabe_erw.Name = "lblReserviert_Ausgabe_erw";
+            this.lblReserviert_Ausgabe_erw.Size = new System.Drawing.Size(126, 13);
+            this.lblReserviert_Ausgabe_erw.TabIndex = 61;
+            this.lblReserviert_Ausgabe_erw.Text = "Reserviert_Ausgabe_erw";
+            // 
+            // btnAendern_erw
+            // 
+            this.btnAendern_erw.Location = new System.Drawing.Point(32, 569);
+            this.btnAendern_erw.Name = "btnAendern_erw";
+            this.btnAendern_erw.Size = new System.Drawing.Size(75, 23);
+            this.btnAendern_erw.TabIndex = 62;
+            this.btnAendern_erw.Text = "Ändern";
+            this.btnAendern_erw.UseVisualStyleBackColor = true;
+            // 
+            // btnLoeschen_erw
+            // 
+            this.btnLoeschen_erw.Location = new System.Drawing.Point(113, 569);
+            this.btnLoeschen_erw.Name = "btnLoeschen_erw";
+            this.btnLoeschen_erw.Size = new System.Drawing.Size(75, 23);
+            this.btnLoeschen_erw.TabIndex = 63;
+            this.btnLoeschen_erw.Text = "Löschen";
+            this.btnLoeschen_erw.UseVisualStyleBackColor = true;
+            // 
+            // btnNeu_erw
+            // 
+            this.btnNeu_erw.Location = new System.Drawing.Point(194, 569);
+            this.btnNeu_erw.Name = "btnNeu_erw";
+            this.btnNeu_erw.Size = new System.Drawing.Size(75, 23);
+            this.btnNeu_erw.TabIndex = 64;
+            this.btnNeu_erw.Text = "Neu";
+            this.btnNeu_erw.UseVisualStyleBackColor = true;
+            // 
+            // btnAusleihen_erw
+            // 
+            this.btnAusleihen_erw.Location = new System.Drawing.Point(403, 569);
+            this.btnAusleihen_erw.Name = "btnAusleihen_erw";
+            this.btnAusleihen_erw.Size = new System.Drawing.Size(75, 23);
+            this.btnAusleihen_erw.TabIndex = 65;
+            this.btnAusleihen_erw.Text = "Ausleihen";
+            this.btnAusleihen_erw.UseVisualStyleBackColor = true;
+            // 
+            // btnZurueck_erw
+            // 
+            this.btnZurueck_erw.Location = new System.Drawing.Point(484, 569);
+            this.btnZurueck_erw.Name = "btnZurueck_erw";
+            this.btnZurueck_erw.Size = new System.Drawing.Size(85, 23);
+            this.btnZurueck_erw.TabIndex = 66;
+            this.btnZurueck_erw.Text = "Zurückgeben";
+            this.btnZurueck_erw.UseVisualStyleBackColor = true;
+            // 
+            // frmHauptfenster_Erweitert
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(884, 620);
+            this.Controls.Add(this.btnZurueck_erw);
+            this.Controls.Add(this.btnAusleihen_erw);
+            this.Controls.Add(this.btnNeu_erw);
+            this.Controls.Add(this.btnLoeschen_erw);
+            this.Controls.Add(this.btnAendern_erw);
+            this.Controls.Add(this.lblReserviert_Ausgabe_erw);
+            this.Controls.Add(this.lblAusgeliehen_Ausgabe_erw);
+            this.Controls.Add(this.lblBenutzer_eingeloggt_erw);
+            this.Controls.Add(this.btnReservierung_loeschen_erw);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.btnReservieren_erw);
+            this.Controls.Add(this.lblReserviert_erw);
+            this.Controls.Add(this.lblAusgeliehen_erw);
+            this.Controls.Add(this.lblisbn_erw);
+            this.Controls.Add(this.lblGenre_erw);
+            this.Controls.Add(this.lbErgebnis_erw);
+            this.Controls.Add(this.btnSuchen_erw);
+            this.Controls.Add(this.btnAusloggen_erw);
+            this.Controls.Add(this.txbSuche_erw);
+            this.Controls.Add(this.lblJahr_erw);
+            this.Controls.Add(this.lblVerlag_erw);
+            this.Controls.Add(this.lblTitel_erw);
+            this.Controls.Add(this.lblAutor_erw);
+            this.Controls.Add(this.lblSuche);
+            this.Controls.Add(this.lblBenutzer_erw);
+            this.Name = "frmHauptfenster_Erweitert";
+            this.Text = "Hauptfenster_Erweitert";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReservieren_erw;
+        private System.Windows.Forms.Label lblReserviert_erw;
+        private System.Windows.Forms.Label lblAusgeliehen_erw;
+        private System.Windows.Forms.Label lblisbn_erw;
+        private System.Windows.Forms.Label lblGenre_erw;
+        private System.Windows.Forms.ListBox lbErgebnis_erw;
+        private System.Windows.Forms.Button btnSuchen_erw;
+        private System.Windows.Forms.Button btnAusloggen_erw;
+        private System.Windows.Forms.TextBox txbSuche_erw;
+        private System.Windows.Forms.Label lblJahr_erw;
+        private System.Windows.Forms.Label lblVerlag_erw;
+        private System.Windows.Forms.Label lblTitel_erw;
+        private System.Windows.Forms.Label lblAutor_erw;
+        private System.Windows.Forms.Label lblSuche;
+        private System.Windows.Forms.Label lblBenutzer_erw;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox6;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.Button btnReservierung_loeschen_erw;
+        private System.Windows.Forms.Label lblBenutzer_eingeloggt_erw;
+        private System.Windows.Forms.Label lblAusgeliehen_Ausgabe_erw;
+        private System.Windows.Forms.Label lblReserviert_Ausgabe_erw;
+        private System.Windows.Forms.Button btnAendern_erw;
+        private System.Windows.Forms.Button btnLoeschen_erw;
+        private System.Windows.Forms.Button btnNeu_erw;
+        private System.Windows.Forms.Button btnAusleihen_erw;
+        private System.Windows.Forms.Button btnZurueck_erw;
+    }
+}
