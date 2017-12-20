d45220ca631be2f068aa04bbc38471c98064ac91
germanflo <wrflorian9@yahoo.de>
2017-12-20 11:24:41 +0100

    SQL script erneuert

--- SQL Script/Insert/insert_verlag.sql
+++ SQL Script/Insert/insert_verlag.sql
@@ -4,10 +4,10 @@ LIMIT 0, 1000
 
 -- Date: 2017-12-15 08:23
 */
-INSERT INTO `buecher_verlage` (`ID`,`Verlag`) VALUES (1,' Aufbau-Verlag ');
-INSERT INTO `buecher_verlage` (`ID`,`Verlag`) VALUES (2,' Bastei Lübbe ');
-INSERT INTO `buecher_verlage` (`ID`,`Verlag`) VALUES (3,' C. Bertelsmann Verlag (Random House) ');
-INSERT INTO `buecher_verlage` (`ID`,`Verlag`) VALUES (4,' Blanvalet Verlag ');
+INSERT INTO `buecher_verlage` (`ID`,`Verlag`) VALUES (1,'Aufbau-Verlag ');
+INSERT INTO `buecher_verlage` (`ID`,`Verlag`) VALUES (2,'Bastei Lübbe ');
+INSERT INTO `buecher_verlage` (`ID`,`Verlag`) VALUES (3,'C. Bertelsmann Verlag (Random House) ');
+INSERT INTO `buecher_verlage` (`ID`,`Verlag`) VALUES (4,'Blanvalet Verlag ');
 INSERT INTO `buecher_verlage` (`ID`,`Verlag`) VALUES (5,'Cora Verlag ');
 INSERT INTO `buecher_verlage` (`ID`,`Verlag`) VALUES (6,'Diogenes');
 INSERT INTO `buecher_verlage` (`ID`,`Verlag`) VALUES (7,'Droemer (Droemer Knaur/Weltbild)');
@@ -20,6 +20,6 @@ INSERT INTO `buecher_verlage` (`ID`,`Verlag`) VALUES (13,'Hoffmann und Campe');
 INSERT INTO `buecher_verlage` (`ID`,`Verlag`) VALUES (14,'Kiepenheuer & Witsch');
 INSERT INTO `buecher_verlage` (`ID`,`Verlag`) VALUES (15,'Piper Verlag ');
 INSERT INTO `buecher_verlage` (`ID`,`Verlag`) VALUES (16,'Rowohlt');
-INSERT INTO `buecher_verlage` (`ID`,`Verlag`) VALUES (17,' Suhrkamp / Insel ');
+INSERT INTO `buecher_verlage` (`ID`,`Verlag`) VALUES (17,'Suhrkamp / Insel ');
 INSERT INTO `buecher_verlage` (`ID`,`Verlag`) VALUES (18,'Ullstein Verlage (Allegria, Claasen, Econ, Li');
 INSERT INTO `buecher_verlage` (`ID`,`Verlag`) VALUES (19,'Goldmann Verlag');
